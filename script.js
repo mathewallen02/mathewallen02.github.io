8142aaf979da693fb419a039527e30b8cc25a03e
mathewallen02 <mathewallenb@gmail.com>
2024-04-15 07:53:52 -0500

    Added smooth scrolling and navbar rework

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('show-nav');
+});
